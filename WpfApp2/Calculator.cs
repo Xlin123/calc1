31b398551929cfb05e3d171423c0d39a45eb3155
Xavier Lin <xavier.lin.1332@gmail.com>
2018-08-19 16:34:24 -0400

    removed unneeded usings

--- WpfApp2/Calculator.cs
+++ WpfApp2/Calculator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfApp2
+﻿namespace WpfApp2
 {
     public class Calculator
     {
