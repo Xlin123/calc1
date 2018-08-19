f4a1d17c19822b37bde09ebae59586e8a5b13745
Xavier Lin <xavier.lin.1332@gmail.com>
2018-08-19 16:21:54 -0400

    refactored so it is more DRY

--- WpfApp2/Calculator.cs
+++ WpfApp2/Calculator.cs
@@ -8,45 +8,67 @@ namespace WpfApp2
 {
     public class Calculator
     {
+        public enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+        }
+
         public static string Multiply(string input1, string input2)
         {
-
-            string answerText = "--";
-
-            if (decimal.TryParse(input1, out decimal a) && decimal.TryParse(input2, out decimal b))
-                answerText = (a * b).ToString();
-
-            return answerText;
+            return Calculate(input1, input2, Operation.Multiply);
         }
 
         public static string Divide(string input1, string input2)
         {
-            string answerText = "--";
-
-            if (decimal.TryParse(input1, out decimal a) && decimal.TryParse(input2, out decimal b) && b != 0)
-                answerText = (a / b).ToString();
-
-            return answerText;
+            return Calculate(input1, input2, Operation.Divide);
         }
 
         public static string Subtract(string input1, string input2)
         {
-            string answerText = "--";
-
-            if (decimal.TryParse(input1, out decimal a) && decimal.TryParse(input2, out decimal b))
-                answerText = (a - b).ToString();
-
-            return answerText;
+            return Calculate(input1, input2, Operation.Subtract);
         }
 
         public static string Add(string input1, string input2)
         {
+            return Calculate(input1, input2, Operation.Add);
+        }
+
+        private static string Calculate(string input1, string input2, Operation operation)
+        {
             string answerText = "--";
 
-            if (decimal.TryParse(input1, out decimal a) && decimal.TryParse(input2, out decimal b))
-                answerText = (a + b).ToString();
+            if (divisorIsZero(input2, operation))
+                return answerText;
 
+            if (decimal.TryParse(input1, out decimal a) && decimal.TryParse(input2, out decimal b))
+            {
+                switch (operation)
+                {
+                    case Operation.Add:
+                        answerText = (a + b).ToString();
+                        break;
+                    case Operation.Subtract:
+                        answerText = (a - b).ToString();
+                        break;
+                    case Operation.Divide:
+                        answerText = (a / b).ToString();
+                        break;
+                    case Operation.Multiply:
+                        answerText = (a * b).ToString();
+                        break;
+                    default:
+                        break;
+                }
+            }
             return answerText;
+        }
+
+        private static bool divisorIsZero(string divisor, Operation operation)
+        {
+            return (decimal.TryParse(divisor, out decimal b) && operation == Operation.Divide && b == 0);
         }
     }
 }
